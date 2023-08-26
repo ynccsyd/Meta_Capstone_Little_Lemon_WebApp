e0f4d6305e8ae2e1194a51d4f9d6d57acde5f52d
ynccsyd <muhibbi_10_@hotmail.com>
2023-08-26 16:06:18 +0300

    Added Layout

--- src/App.js
+++ src/App.js
@@ -1,63 +1,48 @@
-import { useReducer } from 'react';
-import {createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-  Outlet,
+import {
+  Route, 
+  Routes
 } from 'react-router-dom';
-import { AppProvider, ThemeProvider } from './context';
-import { Header, Footer } from './components';
-import './App.css';
-import { Home, BookingPage, ConfirmedBooking } from './pages';
 
-const Root = () => {
+import Layout from './components/layout/Layout';
+// import Home from './components/pages/Home';
+// import Bookings from './components/pages/Bookings';
+// import ConfirmedBooking from './components/pages/Bookings/ConfirmedBooking';
+// import NotFound from './components/pages/NotFound';
+// import UnderConstruction from './components/pages/UnderConstruction';
+// import pages from './utils/pages';
+
+const App = () => {
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <Layout>
+        <Routes>
+          {/* <Route path={pages.get('home').path} element={<Home />} />
+          <Route 
+            path={pages.get('about').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('menu').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path={pages.get('bookings').path} element={<Bookings />} />
+          <Route 
+            path={pages.get('confirmedBooking').path} 
+            element={<ConfirmedBooking />} 
+          />
+          <Route 
+            path={pages.get('orderOnline').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('login').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </Layout>
     </>
   );
 };
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route index element={<Home />} />
-      <Route path="bookings" element={<BookingPage />}>
-        <Route path="thank-you" element={<ConfirmedBooking />} />
-      </Route>
-    </Route>
-  )
-);
-
-function App() {
-  const initialAppState = {
-    previousLocation: ['/'],
-  };
-  const reducer = (state, { type, payload }) => {
-    switch (type) {
-      case 'setPreviousLocation': {
-        return {
-          // ...state,
-          // previousLocation: [...state.previousLocation]
-          //   .slice(0, 1)
-          //   .push(payload),
-        };
-      }
-
-      default:
-        break;
-    }
-  };
-  const [stateGlobal, dispatchGlobal] = useReducer(reducer, initialAppState);
-  return (
-    <AppProvider value={{ stateGlobal, dispatchGlobal }}>
-      <ThemeProvider>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </AppProvider>
-  );
-}
 
 export default App;
